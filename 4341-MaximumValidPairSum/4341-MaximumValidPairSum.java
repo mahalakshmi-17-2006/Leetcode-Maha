76aa17e4acf4e67b232139d70b5fb47daacc6a43
mahalakshmi-17-2006 <mahalakshmi.m01701@gmail.com>
2026-08-11 14:14:16 +0530

    Create 4341-MaximumValidPairSum

---
+++ 4341-MaximumValidPairSum/4341-MaximumValidPairSum.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 2:14:16 PM
+class Solution {
+    public int maxValidPairSum(int[] nums, int k) {
+        int n=nums.length;
+        int maxLeft = nums[0];
+        int ans=Integer.MIN_VALUE;
+        for(int j=k;j<n;j++){
+            maxLeft=Math.max(maxLeft,nums[j-k]);
+            ans=Math.max(ans,maxLeft + nums[j]);
+        }
+        return ans;
+    }
+}
