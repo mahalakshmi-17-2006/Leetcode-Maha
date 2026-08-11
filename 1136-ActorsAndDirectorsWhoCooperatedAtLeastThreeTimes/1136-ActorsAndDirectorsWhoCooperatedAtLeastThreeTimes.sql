4d6318e65cba47d477479f16245eb7947043e03e
mahalakshmi-17-2006 <mahalakshmi.m01701@gmail.com>
2026-08-11 14:18:58 +0530

    Create 1136-ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes

---
+++ 1136-ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes/1136-ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes.sql
@@ -0,0 +1,6 @@
+-- Last updated: 8/11/2026, 2:18:58 PM
+# Write your MySQL query statement below
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
